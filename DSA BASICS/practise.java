318c6639da0b4c596486682128bd22a14478e80f
Nishant Makwana <nishantmakwanacreations@gmail.com>
2025-03-19 18:48:43 +0530

    CHANGES

---
+++ DSA BASICS/practise.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class practise {
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.addFirst(0);
+
+        System.out.println(list);
+
+
+    }
+}
